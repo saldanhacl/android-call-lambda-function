82bdf72a25c53be33fcfd90cce98a6d44d9c6f31
lucas <lucas.saldanha5@gmail.com>
2019-05-22 08:38:32 -0300

    Clean code

--- app/src/main/java/br/com/go5/geopuc/MapsActivity.kt
+++ app/src/main/java/br/com/go5/geopuc/MapsActivity.kt
@@ -2,11 +2,8 @@ package br.com.go5.geopuc
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.pm.PackageManager
 import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
@@ -23,17 +20,17 @@ import com.amazonaws.auth.CognitoCachingCredentialsProvider
 import com.amazonaws.mobileconnectors.lambdainvoker.LambdaFunctionException
 import com.amazonaws.mobileconnectors.lambdainvoker.LambdaInvokerFactory
 import com.amazonaws.regions.Regions
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
+import com.google.android.gms.location.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-    private lateinit var locationManager: LocationManager
+    private var locationRequest = LocationRequest()
+    private lateinit var locationCallback: LocationCallback
     private val REQUEST_LOCATION = 2
     private val IDENTITY_POOL_ID = "us-east-1:82b774e8-babf-4395-879f-089b2a4d105f"
     private lateinit var fusedLocationClient: FusedLocationProviderClient
@@ -41,9 +38,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        getRuntimePermissions()
         setContentView(R.layout.activity_maps)
+
         setupFragment()
+        getRuntimePermissions()
     }
 
     private fun setupFragment() {
@@ -52,106 +50,97 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, LocationListener {
         mapFragment.getMapAsync(this)
     }
 
+
     private fun getRuntimePermissions() {
-        if (ActivityCompat.checkSelfPermission(
-                this, Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            // Check Permissions Now
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                REQUEST_LOCATION
-            )
+        if (isFineLocationPermissionEnabled()) {
+            setupLocationUpdateCallback()
+            startLocationUpdates()
         } else {
-            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-            getMyLocation()
+            requestFineLocationPermission()
         }
+    }
+
+    private fun isFineLocationPermissionEnabled(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            this, Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestFineLocationPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            REQUEST_LOCATION
+        )
+    }
+
+    private fun setupLocationUpdateCallback() {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                moveCameraByLocation(locationResult.lastLocation)
+                invokeLambda(locationResult.lastLocation)
+            }
+        }
+    }
+
+    private fun moveCameraByLocation(location: Location) {
+        val currentLocation = LatLng(location.latitude, location.longitude)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 16.0f))
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() {
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationClient.requestLocationUpdates(locationRequest,
+            locationCallback,
+            null)
     }
 
     @SuppressLint("MissingPermission")
     override fun onMapReady(googleMap: GoogleMap) {
-        setupMap(googleMap)
-        var currentLocation: LatLng
-        locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 500L, 1000f, this)
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener { location : Location? ->
-                if (location != null) {
-                    currentLocation = LatLng(location.latitude, location.longitude)
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 16.0f))
-                }
-            }
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun setupMap(googleMap: GoogleMap) {
         mMap = googleMap
-        mMap.isMyLocationEnabled = true
-    }
-
-    @SuppressLint("MissingPermission")
-    private fun getMyLocation() {
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener { location : Location? ->
-                callLambda(location)
-            }
-    }
-
-    private val locationListener: LocationListener = object : LocationListener {
-        override fun onLocationChanged(location: Location) {
-           callLambda(location)
+        if (isFineLocationPermissionEnabled()) {
+            mMap.isMyLocationEnabled = true
         }
-        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
-        override fun onProviderEnabled(provider: String) {}
-        override fun onProviderDisabled(provider: String) {}
     }
 
-    private fun callLambda(location: Location?) {
+    private fun invokeLambda(location: Location?) {
 
+        val factory = createLambdaFactory()
+
+        doAsync {
+            try {
+                if (location !== null) {
+                    val response = callLambdaFunction(factory, location)
+                    uiThread {
+                        toast(response)
+                    }
+                } else {
+                    throw Exception("Can't get user's location")
+                }
+            } catch (ex: Exception) {
+                Log.e("LAMBDA", "Lambda execution failed: " + ex.message)
+            }
+        }
+    }
+
+    private fun createLambdaFactory(): LambdaInvokerFactory {
         val credentialsProvider = CognitoCachingCredentialsProvider(
             this@MapsActivity.applicationContext,
             IDENTITY_POOL_ID,
             Regions.US_EAST_1)
 
-        val factory = LambdaInvokerFactory(
+        return LambdaInvokerFactory(
             this@MapsActivity.applicationContext,
             Regions.US_EAST_1,
             credentialsProvider)
-
-        doAsync {
-            try {
-                val  myInterface = factory.build(GetDistanceToPucLambdaInterface::class.java)
-                val request = RequestClass(location!!.latitude, location.longitude)
-                val response: String = myInterface.getDistanceToPuc(request)
-                uiThread {
-                    toast(response)
-                }
-            } catch (ex: LambdaFunctionException) {
-                Log.e("LAMBDA", "Lambda execution failed " + ex.details)
-            }
-        }
     }
 
-
-    // LOCATION LISTENERS
-
-    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onProviderEnabled(provider: String?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onProviderDisabled(provider: String?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onLocationChanged(location: Location?) {
-        val currentLocation = LatLng(location!!.latitude, location.longitude)
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 16.0f))
-        callLambda(location)
+    private fun callLambdaFunction(factory: LambdaInvokerFactory, location: Location): String {
+        val  myInterface = factory.build(GetDistanceToPucLambdaInterface::class.java)
+        val request =  RequestClass(location.latitude, location.longitude)
+        return myInterface.getDistanceToPuc(request)
     }
 
 
